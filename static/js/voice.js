38c60209bb78c09d4cfdd5e3c9ccb47071f176ec
ffont <frederic.font@gmail.com>
2015-11-30 12:37:16 +0100

    Added voice control according to google doc specifications

--- static/js/voice.js
+++ static/js/voice.js
@@ -1,86 +1,150 @@
-var create_email = false;
 var final_transcript = '';
 var recognizing = false;
-var ignore_onend;
-var start_timestamp;
+var restart_on_end = true;
+var process_on_end = true;
+var restart_on_error = true;
 
 if (!('webkitSpeechRecognition' in window)) {
-  console.log("Speech recognition not supported...")
+    console.log("Speech recognition not supported...")
 } else {
-  var recognition = new webkitSpeechRecognition();
-  recognition.continuous = false;
-  recognition.interimResults = true;
-  recognition.onstart = function() {
-    recognizing = true;
-  	////alert('info_speak_now');
-  };
-  recognition.onerror = function(event) {
-    if (event.error == 'no-speech') {
-      ////alert('info_no_speech');
-      ignore_onend = true;
-    }
-    if (event.error == 'audio-capture') {
-      ////alert('info_no_microphone');
-      ignore_onend = true;
-    }
-    if (event.error == 'not-allowed') {
-      if (event.timeStamp - start_timestamp < 100) {
-        //alert('info_blocked');
-      } else {
-        //alert('info_denied');
-      }
-      ignore_onend = true;
-    }
-  };
-  recognition.onend = function() {
-    recognizing = false;
-    if (ignore_onend) {
-      return;
-    }
-    if (!final_transcript) {
-      //alert('info_start');
-      return;
-    }
-    //alert('');
-    /*
-    if (window.getSelection) {
-      window.getSelection().removeAllRanges();
-      var range = document.createRange();
-      range.selectNode(document.getElementById('final_span'));
-      window.getSelection().addRange(range);
-    }*/
-  };
-  recognition.onresult = function(event) {
-    var interim_transcript = '';
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
-      } else {
-        interim_transcript += event.results[i][0].transcript;
-      }
-    }
-    console.log("ON RESULT")
-    console.log(final_transcript)
-    console.log(interim_transcript)
-    /*final_transcript = capitalize(final_transcript);
-    final_span.innerHTML = linebreak(final_transcript);
-    interim_span.innerHTML = linebreak(interim_transcript);
-    if (final_transcript || interim_transcript) {
-      showButtons('inline-block');
-    }*/
-  };
+    console.log("Setting up speech recognition...")
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = false;
+    recognition.interimResults = false;
+    recognition.onstart = function() {
+        recognizing = true;
+    };
+    recognition.onerror = function(event) {
+        console.log("Speech, ERROR: " + event.error);
+        if (event.error == 'no-speech') {
+            ////alert('info_no_speech');
+        }
+        if (event.error == 'audio-capture') {
+            ////alert('info_no_microphone');
+            restart_on_error = false;
+        }
+        if (event.error == 'not-allowed') {
+            restart_on_error = false;
+        }
+        if (restart_on_error){
+            startSpeechToText();
+        }
+    };
+    recognition.onend = function() {
+        console.log("Speech, END");
+        recognizing = false;
+        if (restart_on_end){
+            startSpeechToText();
+        }
+        if (process_on_end){
+            processTranscript(final_transcript);
+        }
+    };
+    recognition.onresult = function(event) {
+        console.log("Speech, RESULT");
+        var interim_transcript = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+            } else {
+                interim_transcript += event.results[i][0].transcript;
+            }
+        }
+        recognizing = false;
+        recognition.stop();
+        processTranscript(final_transcript);
+        startSpeechToText();
+    };
 }
 
-function startButton(event) {
-  if (recognizing) {
-    recognition.stop();
-    return;
-  }
-  final_transcript = '';
-  recognition.lang = "en-US";
-  recognition.start();
-  ignore_onend = false;
-  final_span.innerHTML = '';
-  interim_span.innerHTML = '';
-  start_timestamp = event.timeStamp;
+function startSpeechToText(){
+    if (!recognizing) {
+        recognition.lang = "en-US";
+        recognition.start();
+    }
 }
+
+function contains_string_or_strings(string, options){
+    var output = false;
+    for (var i in options){
+        var option = options[i];
+        if (string.indexOf(option) > -1){
+            output = true;
+            break;
+        }
+    }
+    return output;
+}
+
+function get_number_in_transcript(transcript){
+    if (transcript.indexOf("1") > -1){
+        return 1;
+    }
+    if (transcript.indexOf("one") > -1){
+        return 1;
+    }
+    if (transcript.indexOf("2") > -1){
+        return 2;
+    }
+    if (transcript.indexOf("two") > -1){
+        return 2;
+    }
+    if (transcript.indexOf("3") > -1){
+        return 3;
+    }
+    if (transcript.indexOf("three") > -1){
+        return 3;
+    }
+    if (transcript.indexOf("4") > -1){
+        return 4;
+    }
+    if (transcript.indexOf("four") > -1){
+        return 4;
+    }
+    return -1;
+}
+
+
+VOICE_CHANGING_SOUND = false;
+function processTranscript(transcript){
+    transcript = transcript.toLowerCase();
+    console.log("Processing transcript: " + transcript);
+
+    if (!VOICE_CHANGING_SOUND){
+        if (contains_string_or_strings(transcript, ["play"])){
+            start_sequencer();
+        }
+        else if (contains_string_or_strings(transcript, ["stop"])){
+            stop_sequencer();
+        }
+        else if (contains_string_or_strings(transcript, ["record"])){
+            var number = get_number_in_transcript(transcript);
+            if (number > -1) {
+                toggle_recording(number - 1);
+            }
+        }
+        else if (contains_string_or_strings(transcript, ["search"])){
+            var number = get_number_in_transcript(transcript);
+            if (number > -1) {
+                change_sound(number - 1);
+                VOICE_CHANGING_SOUND = true;
+            }
+        }
+        else if (contains_string_or_strings(transcript, ["clear"])){
+            var number = get_number_in_transcript(transcript);
+            if (number > -1) {
+                clear_track(number - 1);
+            }
+        }
+        else if (contains_string_or_strings(transcript, ["tempo"])){
+            start_changing_tempo();
+        }
+    } else {
+        $("#query_terms").val(transcript);
+        load_from_freesound_text_search();
+        VOICE_CHANGING_SOUND = false;
+    }
+    final_transcript = '';
+}
+
+startSpeechToText();
